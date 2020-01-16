636ff48c29be12d4059f6abfe31fa0dd8e546797
Rahul Reddy <ragul880@gmail.com>
2020-01-16 17:56:18 +0530

    second

--- advanced_section/basic_app/urls.py
+++ advanced_section/basic_app/urls.py
@@ -5,5 +5,8 @@ app_name = 'basic_app'
 
 urlpatterns=[
     path('',views.SchoolListView.as_view(),name='list'),
-    re_path(r'^(?P<pk>[-\w]+)/$',views.SchoolDetailView.as_view(),name='detail')
+    path('create/',views.SchoolCreateView.as_view(),name='create'),
+    re_path(r'^(?P<pk>[-\w]+)/$',views.SchoolDetailView.as_view(),name='detail'),
+    re_path(r'^update/(?P<pk>[-\w]+)/$',views.SchoolUpdateView.as_view(),name='update'),
+    re_path(r'^delete/(?P<pk>[-\w]+)/$',views.SchoolDeleteView.as_view(),name='delete'),
 ]
